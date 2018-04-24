d2255ad4b1b68a29d2b615e588c99743d9507f1e
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-04-24 15:13:20 -0400

    in-progress

--- Mucho_groucho.sql
+++ Mucho_groucho.sql
@@ -43,6 +43,25 @@ if(first_name = "MUCHO GROUCHO", "HARPO", first_name)));
 update actor set first_name = case when first_name = "HARPO" then "GROUCHO" when first_name = "GROUCHO" then "MUCHO GROUCHO"
 when first_name = "MUCHO GROUCHO" then "HARPO" else first_name end;
 
+#5a
+show create table address;
+CREATE TABLE `address` (
+  `address_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `address` varchar(50) NOT NULL,
+  `address2` varchar(50) DEFAULT NULL,
+  `district` varchar(20) NOT NULL,
+  `city_id` smallint(5) unsigned NOT NULL,
+  `postal_code` varchar(10) DEFAULT NULL,
+  `phone` varchar(20) NOT NULL,
+  `location` geometry NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`address_id`),
+  KEY `idx_fk_city_id` (`city_id`),
+  SPATIAL KEY `idx_location` (`location`),
+  CONSTRAINT `fk_address_city` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=606 DEFAULT CHARSET=utf8;
+
+
 
 
 
